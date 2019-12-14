dfeefca7e50ddc8ef23109141de750ce8507eb76
mahdihadrich <mahdo.ha@gmail.com>
2019-12-15 00:02:59 +0100

    adding addon views

--- my_addons/hopital/__manifest__.py
+++ my_addons/hopital/__manifest__.py
@@ -25,8 +25,6 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
